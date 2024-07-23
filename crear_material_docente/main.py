bafe2826f0fc5267a72d9926098dd04f9e9503cb
Thrinky_asus <ptrenchs@gmail.com>
2024-07-23 12:45:30 +0200

    Se ha empezado a realizar en el main de la carpeta crear_material_docente los cambios para empezar a hacer el codigo principal

--- crear_material_docente/main.py
+++ crear_material_docente/main.py
@@ -1,5 +1,5 @@
 from fun_CMD import contenido_directorio
-from fun_CMD import convertir
+import json
 
 
 # carpeta = contenido_directorio.Directorio('/home/pol/Escritorio/carpeta_prova').all_archivos()
@@ -9,12 +9,18 @@ from fun_CMD import convertir
 
 # archivos_py = convertir.converitr.ipynb_to_py(archivos_ipynb)
 
-carpeta = contenido_directorio.Directorio('/home/pol/Escritorio/carpeta_prova').all_archivos()
-archivos_py = contenido_directorio.Filtros_formato(rutas = carpeta,formatos = 'py').elejir()
-archivos_py = contenido_directorio.Filtros_carpetas(rutas = archivos_py,carpetas = '.ipynb_checkpoints').eliminar()
-archivos_py = contenido_directorio.ordenar_directorio(rutas=archivos_py).ordenar()
+carpeta = contenido_directorio.Directorio('/home/ptrenchs/Escritorio/carpeta_probas').all_archivos()
+archivos_ipynb = contenido_directorio.Filtros_formato(rutas = carpeta,formatos = 'ipynb').elejir()
+archivos_ipynb = contenido_directorio.Filtros_carpetas(rutas = archivos_ipynb,carpetas = '.ipynb_checkpoints').eliminar()
 
-archivos_ipynb = convertir.converitr.py_to_ipynb(archivos_py)
+with open(archivos_ipynb[0],'r',encoding='utf-8') as archivo:
+    datos = json.load(archivo)
 
-print(archivos_ipynb)
+
+for cell in datos['cells']:
+    if cell['cell_type'] == 'code':
+        for i in range(len(cell['source'])):
+            line = cell['source'][i]
+            
+        print(cell['source'])
 
