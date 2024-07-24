c53d078011ab2dc25cde4d71f3ae5239078d5862
Thrinky_asus <ptrenchs@gmail.com>
2024-07-24 11:30:25 +0200

    Se ha escrito el codigo para crear material de clase y material para los alumnos.

--- crear_material_docente/fun_CMD/fucniones_strings.py
+++ crear_material_docente/fun_CMD/fucniones_strings.py
@@ -4,7 +4,8 @@ class linea:
         self.coment = coment
         self.sep_enter = sep_enter
         self.especiales = especiales
-    def var_indepe(self):
+
+    def tipo_linea_code(self):
         cometario = linea(line=self.line).comentario()
         new_line = self.line.replace(cometario,'')
         for sep in self.sep_enter.split(','):
@@ -19,46 +20,50 @@ class linea:
                 if new_line.startswith(esp):
                     break
             if not new_line.startswith(esp):
-                left = new_line.split('=')[0]
-                rigth = new_line.split('=')[1]
-                try:
-                    eval(rigth)
-                    return self.line.replace(cometario,'')
-                except:
-                    pass
+                if '=' in new_line:
+                    left = new_line.split('=')[0]
+                    rigth = new_line.split('=')[1]
+                    try:
+                        eval(rigth)
+                        return 'indep'
+                    except:
+                        return 'dep'
+                else:
+                    return 'display'
             else:
-                return ''
+                return 'Esepecial'
 
+        else:
+            return ''
+
+    def var_indepe(self):
+        if linea(line=self.line).tipo_linea_code() == 'indep':
+            cometario = linea(line=self.line).comentario()
+            return self.line.replace(cometario,'')
         else:
             return ''
         
+        
     def var_dep(self):
-        cometario = linea(line=self.line).comentario()
-        new_line = self.line.replace(cometario,'')
-        for sep in self.sep_enter.split(','):
-            new_line = new_line.replace(sep,'')
-        if new_line != '':
-            new_line = self.line.replace(cometario,'')
-            for i,car in enumerate(new_line):
-                if not (car in self.sep_enter.split(',')[:-1]):
-                    break
-            new_line = new_line[i:]
-            for esp in self.especiales.split(','):
-                if new_line.startswith(esp):
-                    break
-            if not new_line.startswith(esp):
-                # left = new_line.split('=')[0]
-                rigth = new_line.split('=')[1]
-                try:
-                    eval(rigth)
-                    return ''
-                except:
-                    pass
-            else:
-                return self.line.replace(cometario,'')
-
+        if linea(line=self.line).tipo_linea_code() == 'dep':
+            cometario = linea(line=self.line).comentario()
+            return self.line.replace(cometario,'')
         else:
             return ''
+        
+    def line_especial(self):
+        if linea(line=self.line).tipo_linea_code() == 'Esepecial':
+            cometario = linea(line=self.line).comentario()
+            return self.line.replace(cometario,'')
+        else:
+            return ''
+    def ver_valor(self):
+        if linea(line=self.line).tipo_linea_code() == 'display':
+            cometario = linea(line=self.line).comentario()
+            return self.line.replace(cometario,'')
+        else:
+            return ''
+
 
     def comentario(self):
         for i,car in enumerate(self.line):
@@ -76,4 +81,4 @@ class linea:
         else:
             return ''
         
-# print(linea(line='x = 1 # hola').var_dep())
+# print(linea(line=' #     hola').comentario())
--- crear_material_docente/main.py
+++ crear_material_docente/main.py
@@ -1,5 +1,7 @@
 from fun_CMD import contenido_directorio
+from fun_CMD import fucniones_strings
 import json
+import os
 
 
 # carpeta = contenido_directorio.Directorio('/home/pol/Escritorio/carpeta_prova').all_archivos()
@@ -13,33 +15,55 @@ carpeta = contenido_directorio.Directorio('/home/ptrenchs/Escritorio/carpeta_pro
 archivos_ipynb = contenido_directorio.Filtros_formato(rutas = carpeta,formatos = 'ipynb').elejir()
 archivos_ipynb = contenido_directorio.Filtros_carpetas(rutas = archivos_ipynb,carpetas = '.ipynb_checkpoints').eliminar()
 
-with open(archivos_ipynb[0],'r',encoding='utf-8') as archivo:
-    datos = json.load(archivo)
 
+## bucle for
+ruta_archivo = archivos_ipynb[0]
+with open(ruta_archivo,'r',encoding='utf-8') as archivo:
+    datos_clase = json.load(archivo)
+with open(ruta_archivo,'r',encoding='utf-8') as archivo:
+    datos_alumno = json.load(archivo)
 
-for cell in datos['cells']:
+nom_format = os.path.basename(ruta_archivo)
+ruta_carp_principal = '/'.join(ruta_archivo.split('/')[:-2])
+
+for pos,cell in enumerate(datos_alumno['cells']):
     if cell['cell_type'] == 'code':
-        # Caracteres que se entienden como comentarios
-        coment = '#'
+        datos_clase['cells'][pos]['execution_count'] = None
+        datos_clase['cells'][pos]['outputs'] = []
+
+        datos_alumno['cells'][pos]['execution_count'] = None
+        datos_alumno['cells'][pos]['outputs'] = []
+        v_d_0 = ''
+        v_i_0 = ''
         for i in range(len(cell['source'])):
             line = cell['source'][i]
-            for j,caracter in enumerate(line):
-                for cm in coment.split(','):
-                    if cm == caracter:
-                        break
-                if cm == caracter:
-                    break
-            if cm == caracter:
-                sep_enter = ' ,\t,\n'
-                principio = line[:j]
-                coment = line[j:]
-                for sep in sep_enter.split(','):
-                    principio = principio.replace(sep,'')
-                if principio != '':
-                    left_left = principio.split('=')[0]
-                    rigth_rigth = principio.split('=')[1]
-                    eval()
+            line_class= fucniones_strings.linea(line=line)
+            com,v_i, v_d, l_e, v_v = [line_class.comentario(), line_class.var_indepe(), line_class.var_dep(), line_class.line_especial(),line_class.ver_valor()]
+            if 'import' in v_v:
+                datos_alumno['cells'][pos]['source'][i] =  v_v
             else:
+                if v_i_0 != '' and v_v != '':
+                    datos_alumno['cells'][pos]['source'][i] =  v_v
+                else:
+                    if v_d != '':
+                        left = ((v_d.split('=')[0]).replace('\t','')).replace(' ','')
+                        datos_alumno['cells'][pos]['source'][i] = com + v_i + f'# Encuentra variable que debe ser {left}\n'
+                    else:
+                        datos_alumno['cells'][pos]['source'][i] = com + v_i
+            v_i_0 = v_i
+        # print(cell['source'])
+# print(datos_alumno['cells'])
+os.makedirs(ruta_carp_principal + '/2-carpeta_clase', exist_ok=True)
+ruta_clase = ruta_carp_principal + '/2-carpeta_clase' + '/' + nom_format
 
-        print(cell['source'])
+with open(ruta_clase, 'w', encoding='utf-8') as nuevo_archivo:
+    json.dump(datos_clase, nuevo_archivo, ensure_ascii=False, indent=4)
+
+
+os.makedirs(ruta_carp_principal + '/3-carpeta_alumnos', exist_ok=True)
+ruta_alumnos = ruta_carp_principal + '/3-carpeta_alumnos' + '/' + nom_format
+
+with open(ruta_alumnos, 'w', encoding='utf-8') as nuevo_archivo:
+    json.dump(datos_alumno, nuevo_archivo, ensure_ascii=False, indent=4)
+
 
