30be11233ece535e869eef88224807997dd82d57
Thrinky_asus <ptrenchs@gmail.com>
2024-07-29 09:14:14 +0200

    Se ha actualizado el codigo para que sea mas eficiente y visual

--- crear_material_docente/fun_CMD/manipulacion_archivos.py
+++ crear_material_docente/fun_CMD/manipulacion_archivos.py
@@ -2,28 +2,24 @@ from fun_CMD import fucniones_strings
 import json
 import os
 
-class Enunciados:
+class Material:
     def __init__(self, rutas, corregir = False):
         self.rutas = rutas
         self.corregir = corregir
 
-    def crear_enunciados_clase_alumno(self):
+    def Enunciados(self):
         for ruta_archivo in self.rutas:
             with open(ruta_archivo,'r',encoding='utf-8') as archivo:
-                datos_clase = json.load(archivo)
-            with open(ruta_archivo,'r',encoding='utf-8') as archivo:
-                datos_alumno = json.load(archivo)
+                datos_enuciado = json.load(archivo)
 
             nom_format = os.path.basename(ruta_archivo)
             ruta_carp_principal = '/'.join(ruta_archivo.split('/')[:-2])
 
-            for pos,cell in enumerate(datos_alumno['cells']):
+            for pos,cell in enumerate(datos_enuciado['cells']):
                 if cell['cell_type'] == 'code':
-                    datos_clase['cells'][pos]['execution_count'] = None
-                    datos_clase['cells'][pos]['outputs'] = []
 
-                    datos_alumno['cells'][pos]['execution_count'] = None
-                    datos_alumno['cells'][pos]['outputs'] = []
+                    datos_enuciado['cells'][pos]['execution_count'] = None
+                    datos_enuciado['cells'][pos]['outputs'] = []
                     v_d_0 = ''
                     v_i_0 = ''
                     for i in range(len(cell['source'])):
@@ -31,36 +27,55 @@ class Enunciados:
                         line_class= fucniones_strings.linea(line=line)
                         com,v_i, v_d, l_e, v_v = [line_class.comentario(), line_class.var_indepe(), line_class.var_dep(), line_class.line_especial(),line_class.ver_valor()]
                         if 'import' in v_v:
-                            datos_alumno['cells'][pos]['source'][i] =  v_v
+                            datos_enuciado['cells'][pos]['source'][i] =  v_v
                         else:
                             if v_i_0 != '' and v_v != '':
-                                datos_alumno['cells'][pos]['source'][i] =  v_v
+                                datos_enuciado['cells'][pos]['source'][i] =  v_v
                             else:
                                 if v_d != '':
                                     left = ((v_d.split('=')[0]).replace('\t','')).replace(' ','')
-                                    datos_alumno['cells'][pos]['source'][i] = com + v_i + f'# Encuentra variable que debe ser {left}\n'
-                                    # datos_clase['cells'][pos]['source'][i] = com + v_i + f'# Encuentra variable que debe ser {left}\n' + v_d
+                                    datos_enuciado['cells'][pos]['source'][i] = com + v_i + f'# Encuentra variable que debe ser {left}\n'
+                                    # datos_respuestas['cells'][pos]['source'][i] = com + v_i + f'# Encuentra variable que debe ser {left}\n' + v_d
                                 else:
-                                    datos_alumno['cells'][pos]['source'][i] = com + v_i
+                                    datos_enuciado['cells'][pos]['source'][i] = com + v_i
                         v_i_0 = v_i
-                    # print(cell['source'])
-            # print(datos_alumno['cells'])
-            os.makedirs(ruta_carp_principal + '/2-ejercicio_resuelto', exist_ok=True)
-            ruta_clase = ruta_carp_principal + '/2-ejercicio_resuelto' + '/' + nom_format
 
-            with open(ruta_clase, 'w', encoding='utf-8') as nuevo_archivo:
-                json.dump(datos_clase, nuevo_archivo, ensure_ascii=False, indent=4)
 
 
             os.makedirs(ruta_carp_principal + '/3-enunciados', exist_ok=True)
-            ruta_alumnos = ruta_carp_principal + '/3-enunciados' + '/' + nom_format
+            ruta_enucnados = ruta_carp_principal + '/3-enunciados' + '/' + nom_format
 
-            with open(ruta_alumnos, 'w', encoding='utf-8') as nuevo_archivo:
-                json.dump(datos_alumno, nuevo_archivo, ensure_ascii=False, indent=4)
+            with open(ruta_enucnados, 'w', encoding='utf-8') as nuevo_archivo:
+                json.dump(datos_enuciado, nuevo_archivo, ensure_ascii=False, indent=4)
             if self.corregir:
                 os.makedirs(ruta_carp_principal + '/4-resuelto_Alumnos', exist_ok=True)
+    
+    def ejercicio_resuelto(self):
+        for ruta_archivo in self.rutas:
+            with open(ruta_archivo,'r',encoding='utf-8') as archivo:
+                datos_respuestas = json.load(archivo)
 
-class Corregir:
-    def __init__(self, rutas, corregir = False):
-        self.rutas = rutas
-        self.corregir = corregir
+            nom_format = os.path.basename(ruta_archivo)
+            ruta_carp_principal = '/'.join(ruta_archivo.split('/')[:-2])
+
+            for pos,cell in enumerate(datos_respuestas['cells']):
+                if cell['cell_type'] == 'code':
+                    datos_respuestas['cells'][pos]['execution_count'] = None
+                    datos_respuestas['cells'][pos]['outputs'] = []
+
+                    for i in range(len(cell['source'])):
+                        line = cell['source'][i]
+                        line_class= fucniones_strings.linea(line=line)
+                        com,v_i, v_d, l_e, v_v = [line_class.comentario(), line_class.var_indepe(), line_class.var_dep(), line_class.line_especial(),line_class.ver_valor()]
+                        
+                        if v_d != '':
+                            left = ((v_d.split('=')[0]).replace('\t','')).replace(' ','')
+                            datos_respuestas['cells'][pos]['source'][i] = com + f'# Encuentra variable que debe ser {left}\n' + v_d
+                        else:
+                            datos_respuestas['cells'][pos]['source'][i] = com + v_i + v_v
+
+            os.makedirs(ruta_carp_principal + '/2-ejercicio_resuelto', exist_ok=True)
+            ruta_respuestas = ruta_carp_principal + '/2-ejercicio_resuelto' + '/' + nom_format
+
+            with open(ruta_respuestas, 'w', encoding='utf-8') as nuevo_archivo:
+                json.dump(datos_respuestas, nuevo_archivo, ensure_ascii=False, indent=4)
--- crear_material_docente/main.py
+++ crear_material_docente/main.py
@@ -5,7 +5,8 @@ from fun_CMD import manipulacion_archivos
 
 carpeta = contenido_directorio.Directorio('/home/ptrenchs/Escritorio/carpeta_probas').all_archivos()
 archivos_ipynb = contenido_directorio.Filtros_formato(rutas = carpeta,formatos = 'ipynb').elejir()
-archivos_ipynb = contenido_directorio.Filtros_carpetas(rutas = archivos_ipynb,carpetas = '.ipynb_checkpoints,2-carpeta_clase,3-carpeta_enunciados,4-corregir').eliminar()
-manipulacion_archivos.Enunciados(rutas=archivos_ipynb,corregir=True).crear_enunciados_clase_alumno()
+archivos_ipynb = contenido_directorio.Filtros_carpetas(rutas = archivos_ipynb,carpetas = '.ipynb_checkpoints,2-ejercicio_resuelto,3-enunciados,4-resuelto_Alumnos').eliminar()
+crear_material = manipulacion_archivos.Material(rutas=archivos_ipynb,corregir=True)
 
-
+crear_material.Enunciados()
+crear_material.ejercicio_resuelto()
